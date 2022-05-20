aabb1d053ecfd84e5a1e2bef1719952bd22402e1
Vadim Hryn <gryn.vadim@gmail.com>
2022-05-20 00:07:13 -0400

    connect to firebase

--- components/Chat.js
+++ components/Chat.js
@@ -11,46 +11,87 @@ export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
+      uid: 0,
       messages: [],
+      user: {
+        _id: "",
+        name: "",
+        avatar: "",
+      },
     }
     if (!firebase.apps.length) {
       firebase.initializeApp({
-        apiKey: "AIzaSyCLPfmMXOuAP7f0SHD3fz6MY7AXBESQGm0",
-        authDomain: "chatapp-4434b.firebaseapp.com",
-        projectId: "chatapp-4434b",
-        storageBucket: "chatapp-4434b.appspot.com",
-        messagingSenderId: "192183344563",
-        appId: "1:192183344563:web:cf6dcc84103f8e32e13fc6",
-        measurementId: "G-ZDVSBVRK91"
+        apiKey: "AIzaSyA0Kq3yc5Qaq5JCfdfrTN5wK5GQstAQOsM",
+        authDomain: "chatapp-13bbe.firebaseapp.com",
+        projectId: "chatapp-13bbe",
+        storageBucket: "chatapp-13bbe.appspot.com",
+        messagingSenderId: "326906990328",
+        appId: "1:326906990328:web:3d0a527aea015a4780f50d",
+        measurementId: "G-0R6VL3WDX0"
       });
     }
     this.referenceChatMessages = firebase.firestore().collection("messages");
-    
+    this.referenceMessagesUser = null;
+
   }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt:  data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar
+        },
+      });
+    });
+    this.setState({
+      messages: messages,
+    });
+  };
   
 
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello developer',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: 'This is a system message',
-          createdAt: new Date(),
-          system: true,
-         },
-      ],
-    })
-  }
+    let name = this.props.route.params.name;
+
+    this.props.navigation.setOptions({ title: name });
+
+    this.unsubscribe = this.referenceChatMessages
+    .orderBy("createdAt", "desc")
+    .onSnapshot(this.onCollectionUpdate);
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+          avatar: "https://placeimg.com/140/140/any"
+        }
+      });
+      this.referenceMessagesUser = firebase.firestore().collection('messages').where("uid", "==", this.state.uid);
+    });
+  
+  
+
+}
+
+ componentWillUnmount() {
+  this.authUnsubscribe();
+  this.unsubscribe();
+}
+
 
   
 
@@ -61,22 +102,40 @@ export default class Chat extends React.Component {
         wrapperStyle={{
           right: {
             backgroundColor: '#072759'
+          },
+          left: {
+            backgroundColor: '#fff'
           }
         }}
       />
     )
   }
 
+  addMessage() {
+    // add a new message to the firebase collection
+    const message = this.state.messages[0];
+
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      createdAt: message.createdAt,
+      text: message.text || '',
+      user: this.state.user,
+    });
+  };
+
   onSend(messages = []) {
+    
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    }),() => {
+      // add messages to local AsyncStorage
+      this.addMessage();
+    })
   }
 
   render() {
-    let name = this.props.route.params.name;
-
-    this.props.navigation.setOptions({ title: name });
+    
     let { bgColor } = this.props.route.params;
     return (
       <View style={{ flex: 1}}>
@@ -84,7 +143,11 @@ export default class Chat extends React.Component {
         renderBubble={this.renderBubble.bind(this)}
         messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
-        user={{_id: 1,}}
+        user={{
+          _id: this.state.user._id,
+          name: this.state.name,
+          avatar: this.state.user.avatar,
+        }}
         />
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null} 
         </View>
